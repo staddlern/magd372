d59ca3f247b73bfe485700998cf44faa7f95b615
staddlern <staddlern@gmail.com>
2023-01-27 12:09:19 -0600

    Implemented Terrain and Player Movement

---
+++ MAGD-372 Assignments/Assets/playermovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 0.03f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = 0f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.right * x) + (transform.forward * z);
+
+        controller.Move(move * speed);
+
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -0.0f * gravity);
+        }
+
+        velocity.y += gravity + Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
